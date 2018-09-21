018627c91a6ce01eaea3794bf53ab7d099b1f182
donatPeter <peterdonat.bme@gmail.com>
2018-09-21 11:03:54 +0200

    use class syntax

--- src/step_definitions/actions.js
+++ src/step_definitions/actions.js
@@ -1,10 +1,12 @@
 const { By, until } = require('selenium-webdriver');
 
-const click = async function (id) {
-  await this.world.driver.wait(until.elementLocated(By.id(id)), 10000, 'Could not locate the child element within the time specified');
-  return this.world.driver.findElement(By.id(id)).click();
-};
+class Actions {
+  static async click(id) {
+    await this.world.driver.wait(until.elementLocated(By.id(id)), 10000, 'Could not locate the child element within the time specified');
+    return this.world.driver.findElement(By.id(id)).click();
+  }
+}
 
 module.exports = [
-  [/I click on the HTML element with the ID "([^"]*)"/, click],
+  [/I click on the HTML element with the ID "([^"]*)"/, Actions.click],
 ];
